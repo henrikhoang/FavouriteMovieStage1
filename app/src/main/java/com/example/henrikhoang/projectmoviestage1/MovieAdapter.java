316a7485ba4b7311b6c0677e6ccaf8d1edd07750
henrikhoang <henrikhoang17@gmail.com>
2017-06-01 17:55:30 -0400

    check

--- app/src/main/java/com/example/henrikhoang/projectmoviestage1/MovieAdapter.java
+++ app/src/main/java/com/example/henrikhoang/projectmoviestage1/MovieAdapter.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import static com.example.henrikhoang.projectmoviestage1.R.styleable.RecyclerView;
+
 /**
  * Created by henrikhoang on 5/25/17.
  */
